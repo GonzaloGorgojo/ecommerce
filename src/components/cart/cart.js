292ec5a906478dc3ff795ad13611a9bda6df4572
Gonzalo Gorgojo <gongorgojo@gmail.com>
2020-12-04 23:59:20 -0300

    rendering condicional

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,16 +1,33 @@
-import React from 'react'
-import useCartContext from '../../context/cartcontext'
+import React from "react";
+import useCartContext from "../../context/cartcontext";
+import {Link} from 'react-router-dom' 
 
+export default function Cart() {
+  const { numero, producto } = useCartContext();
 
-export default function Cart(){
+  const MuestraCompra = ({ condition }) => {
+    condition = numero;
 
-    const {numero, producto} = useCartContext()
-    
-
+    if (condition === 0) {
+      return (
+        <div id="contenedorCarritoVacio">
+          <p>
+            Usted No tiene Items En el Carrito
+          </p>
+          <Link to ={`/`} style={{ textDecoration: 'none' }}>
+      <h4>Vuelva a La Lista de Productos</h4>
+      </Link>
+        </div>
+      );
+    }
     return (
-     <div id="contenedorCarrito">
-       <p>Usted va a comprar {numero} unidades de {producto} </p>
-     </div>
-     
-   )
- }
+      <div id="contenedorCarrito">
+        <p>
+          Usted va a comprar {numero} unidades de {producto}{" "}
+        </p>
+      </div>
+    );
+  };
+
+  return <MuestraCompra />;
+}
