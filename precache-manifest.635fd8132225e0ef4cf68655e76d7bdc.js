2f1c93a835fbfbe805a36074df60a2d060f03fae
Gonzalo Gorgojo <gongorgojo@gmail.com>
2020-12-08 01:49:10 -0300

    Updates

---
+++ precache-manifest.635fd8132225e0ef4cf68655e76d7bdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250410d6700a0e5d4fda9f2eeafa2693",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "d39a18480934f7aa1255",
+    "url": "/ecommerce/static/css/main.dbbcecdb.chunk.css"
+  },
+  {
+    "revision": "eaa5d4d3459ba5e0a290",
+    "url": "/ecommerce/static/js/2.5198c53f.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/ecommerce/static/js/2.5198c53f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d39a18480934f7aa1255",
+    "url": "/ecommerce/static/js/main.9353f905.chunk.js"
+  },
+  {
+    "revision": "831a87ab3375c7c427c2",
+    "url": "/ecommerce/static/js/runtime-main.ee40b3dc.js"
+  }
+]);
